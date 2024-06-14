b50f5f30d79c33febd125e4ec17d56d9ba6ada89
Carlos Colina <carlosecolina89@gmail.com>
2024-06-14 14:41:35 -0500

    csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         'buscarHabitaciones',
-        'buscarArea'
+        'buscarArea',
+        'admin/subcategoria/obtenerd'
     ];
 }
