34af36e098eb5cb667e049829d75213be04ca68a
Carlos Colina <carlosecolina89@gmail.com>
2024-06-20 16:37:26 -0500

    actualizacion cover

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -88,7 +88,7 @@ class ServiceController extends Controller
     {
       $manager = new ImageManager(new Driver());
       $img = $manager->read($file);
-        $img->coverDown(620, 620, 'center');
+        $img->coverDown(842, 910, 'center');
 
       if (!file_exists($route)) {
         mkdir($route, 0777, true); // Se crea la ruta con permisos de lectura, escritura y ejecución
